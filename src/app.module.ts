262c2e68c7eb1567f6ce75834c8c619dbf3dd3db
Faith Mosonik <mosonikchep@gmail.com>
2025-06-12 13:35:52 +0300

    feat: update AppModule to include SolarDetectionModule and additional services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SolarDetectionModule } from './solar-detection/solar-detection.module';
+import { ImageProcessingService } from './image-processing/image-processing.service';
+import { SatelliteImageryService } from './satellite-imagery/satellite-imagery.service';
 
 @Module({
-  imports: [],
+  imports: [SolarDetectionModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ImageProcessingService, SatelliteImageryService],
 })
 export class AppModule {}
